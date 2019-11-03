25b22d094575eb41ec476e64fe095f5b8965050b
Jia Chen <grievejia@gmail.com>
2019-11-02 21:35:11 -0700

    Previously we were not checking fixpoint convergence in houdini solver correctly: If solving predicate A depends on solving predicate B, then whenever the solution for B changes, we should re-solve A again. But B may be a connected component itself and we may perform sub-fixpoint for it -- The solution change detection for B were based on whether the *final* iteration of the sub-fixpoint converges or not, which is wrong -- as long as *any* iteration of the sub-fixpoint doesn't converge, it should count as a solution change for the outer fixpoint.
    While a several-line small fix is possible, I took the chance to rewrite
    the fixpoint loop, as the original logic was rather convoluted -- it
    conflated what the solution was with whether the solution changed or not. The new loop is taken from the [chaotic iteration algorithm](https://github.com/backtracking/ocamlgraph/blob/master/src/chaoticIteration.ml) implemented in `ocamlgraph`, which I presume should be more reliable than the original version which came from mostly me dog-sciencing.

--- src/lib/solver/houdini.ml
+++ src/lib/solver/houdini.ml
@@ -16,14 +16,16 @@ module DepGraph = struct
     add_edge graph root_node false_node ;
     let preds = HornAst.preds_of ast in
     List.iter preds ~f:(fun pred ->
-        add_edge graph root_node pred.HornAst.Predicate.name ) ;
+        add_edge graph root_node pred.HornAst.Predicate.name) ;
     let rules = HornAst.rules_of ast in
-    let process_rule (rule: HornAst.Rule.t) =
+    let process_rule (rule : HornAst.Rule.t) =
       let head_name =
         let open HornAst.Rule.Head in
         match rule.head with
-        | False -> false_node
-        | Predicate {name; _} -> name
+        | False ->
+            false_node
+        | Predicate {name; _} ->
+            name
       in
       let preds = HornAst.Rule.Bodies.predicates_of rule.bodies in
       List.iter preds ~f:(fun pred -> add_edge graph pred head_name)
@@ -35,10 +37,10 @@ end
 module WeakTopo = Graph.WeakTopological.Make (DepGraph)
 
 let init_solution table preds =
-  List.iter preds ~f:(fun (pred: HornAst.Predicate.t) ->
+  List.iter preds ~f:(fun (pred : HornAst.Predicate.t) ->
       let pred_name = pred.name in
       let pred_candidates = pred.hints in
-      SolutionTable.set table ~key:pred_name ~data:pred_candidates )
+      SolutionTable.set table ~key:pred_name ~data:pred_candidates)
 
 let update_solution table pred keep_set =
   SolutionTable.update table pred ~f:(function
@@ -54,22 +56,19 @@ let update_solution table pred keep_set =
           else
             List.filteri candidates ~f:(fun idx _ -> Hash_set.mem keep_set idx)
         in
-        new_candidates )
+        new_candidates)
 
 module InternalStatus = struct
   type t = Verified | Rejected | Undeterminted
 
-  let to_external_status = function
-    | Verified -> Status.Verified
-    | _ ->
-        (* Houdini verifier can never reject vstate because of its incomplete nature *)
-        Status.Unknown
-
   let merge s0 s1 =
     match (s0, s1) with
-    | Rejected, _ | _, Rejected -> Rejected
-    | Verified, Verified -> Verified
-    | _ -> Undeterminted
+    | Rejected, _ | _, Rejected ->
+        Rejected
+    | Verified, Verified ->
+        Verified
+    | _ ->
+        Undeterminted
 end
 
 module Discharger = struct
@@ -77,14 +76,16 @@ module Discharger = struct
     type t = Verified | Rejected | Pruned of Int.Hash_set.t
   end
 
-  let discharge (z3env: Z3Env.t) pre_conds opt_post_conds =
+  let discharge (z3env : Z3Env.t) pre_conds opt_post_conds =
     (* We want to check the validity of "conjunct(pres) ==> conjunct(posts)" *)
     let z3_pres = List.map pre_conds ~f:(Z3Encoder.encode_expr z3env) in
     let z3_pre = Z3.Boolean.mk_and z3env.ctx z3_pres in
     let z3_posts =
       match opt_post_conds with
-      | None -> [Z3.Boolean.mk_false z3env.ctx]
-      | Some post_conds -> List.map post_conds ~f:(Z3Encoder.encode_expr z3env)
+      | None ->
+          [Z3.Boolean.mk_false z3env.ctx]
+      | Some post_conds ->
+          List.map post_conds ~f:(Z3Encoder.encode_expr z3env)
     in
     let z3_query =
       let open Z3.Boolean in
@@ -95,14 +96,18 @@ module Discharger = struct
     let solver = Z3Env.mk_solver z3env in
     add solver [z3_query] ;
     match check solver [] with
-    | UNSATISFIABLE -> Status.Verified
-    | UNKNOWN -> Status.Rejected
-    | SATISFIABLE ->
+    | UNSATISFIABLE ->
+        Status.Verified
+    | UNKNOWN ->
+        Status.Rejected
+    | SATISFIABLE -> (
       match opt_post_conds with
-      | None -> Status.Rejected
-      | Some post_conds ->
+      | None ->
+          Status.Rejected
+      | Some post_conds -> (
         match get_model solver with
-        | None -> Status.Rejected
+        | None ->
+            Status.Rejected
         | Some model ->
             let keep_set =
               Int.Hash_set.create ~size:(List.length post_conds) ()
@@ -111,11 +116,15 @@ module Discharger = struct
                 let query = Z3.Boolean.mk_implies z3env.ctx z3_pre z3_post in
                 (* Set model_completion to true and hope we get a concrete boolean value *)
                 match Z3.Model.eval model query true with
-                | Some e when Z3.Boolean.is_true e -> Hash_set.add keep_set idx
-                | _ -> (* Be conservative and filter out this expression *)
-                       () ) ;
-            Status.Pruned keep_set
+                | Some e when Z3.Boolean.is_true e ->
+                    Hash_set.add keep_set idx
+                | _ ->
+                    (* Be conservative and filter out this expression *)
+                    ()) ;
+            Status.Pruned keep_set ) )
 end
+
+exception SolutionRejected
 
 let run_houdini z3env ast =
   let solution_table = SolutionTable.create ~size:(HornAst.num_preds ast) () in
@@ -128,7 +137,7 @@ let run_houdini z3env ast =
     let subst_map =
       Identifier.Map.of_alist_exn
         (List.map (List.zip_exn pred.params args) ~f:(fun (param, arg) ->
-             (param.VarBinding.name, arg) ))
+             (param.VarBinding.name, arg)))
     in
     List.map exprs ~f:(Expr.subst ~f:(Identifier.Map.find subst_map))
   in
@@ -145,7 +154,8 @@ let run_houdini z3env ast =
         List.fold rules ~init:InternalStatus.Verified ~f:(fun acc rule ->
             let open InternalStatus in
             match acc with
-            | Rejected -> acc
+            | Rejected ->
+                acc
             | _ ->
                 let pre_conds =
                   HornAst.Rule.Bodies.subst_predicate ~f:predicate_subst
@@ -154,7 +164,8 @@ let run_houdini z3env ast =
                 let opt_post_conds =
                   let open HornAst.Rule.Head in
                   match rule.head with
-                  | False -> None
+                  | False ->
+                      None
                   | Predicate {name; args} ->
                       let conds = predicate_lookup name args in
                       Some conds
@@ -162,42 +173,53 @@ let run_houdini z3env ast =
                 let rule_status =
                   let open Discharger in
                   match discharge z3env pre_conds opt_post_conds with
-                  | Status.Verified -> InternalStatus.Verified
-                  | Status.Rejected -> InternalStatus.Rejected
+                  | Status.Verified ->
+                      InternalStatus.Verified
+                  | Status.Rejected ->
+                      InternalStatus.Rejected
                   | Status.Pruned keep_set ->
                       update_solution solution_table pred keep_set ;
                       InternalStatus.Undeterminted
                 in
-                merge acc rule_status )
+                merge acc rule_status)
   in
-  let rec iterate_until_fixpoint acc elem =
-    match acc with
-    | InternalStatus.Rejected -> acc
-    | _ ->
-        let open Graph.WeakTopological in
-        let open InternalStatus in
-        let elem_status =
-          match elem with
-          | Vertex v -> process_pred v
-          | Component (v, elem') ->
-              let rec iterate () =
-                let vstatus = process_pred v in
-                match fold_left iterate_until_fixpoint vstatus elem' with
-                | (Rejected | Verified) as s -> s
-                | Undeterminted -> iterate ()
-              in
-              iterate ()
-        in
-        merge acc elem_status
+  let rec iterate_components components =
+    Graph.WeakTopological.fold_left iterate_component () components
+  and iterate_component _ component =
+    let open Graph.WeakTopological in
+    match component with
+    | Component (leader, components) ->
+        stablize false leader components
+    | Vertex v -> (
+      match process_pred v with
+      | InternalStatus.Rejected ->
+          raise SolutionRejected
+      | _ ->
+          () )
+  and stablize can_stop leader components =
+    let solution_changed =
+      match process_pred leader with
+      | InternalStatus.Rejected ->
+          raise SolutionRejected
+      | InternalStatus.Verified ->
+          false
+      | InternalStatus.Undeterminted ->
+          true
+    in
+    if can_stop && not solution_changed then ()
+    else (
+      iterate_components components ;
+      stablize true leader components )
   in
-  let internal_status =
-    Graph.WeakTopological.fold_left iterate_until_fixpoint
-      InternalStatus.Verified weak_topo
-  in
-  Logs.debug (fun m ->
-      SolutionTable.iteri solution_table ~f:(fun ~key ~data ->
-          let pp_and fmt () = Fmt.string fmt " && " in
-          m " Final Houdini solution for %a: %a" Identifier.pp key
-            (Fmt.list ~sep:pp_and Expr.pp)
-            data ) ) ;
-  InternalStatus.to_external_status internal_status
+  try
+    iterate_components weak_topo ;
+    SolutionTable.iteri solution_table ~f:(fun ~key ~data ->
+        let pp_and fmt () = Fmt.string fmt " && " in
+        Logs.info (fun m ->
+            m "Final solution for %a: %a" Identifier.pp key
+              (Fmt.list ~sep:pp_and Expr.pp)
+              data)) ;
+    Status.Verified
+  with SolutionRejected ->
+    (* Houdini verifier can never reject vstate because of its incomplete nature *)
+    Status.Unknown
